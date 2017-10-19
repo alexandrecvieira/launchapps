9193dcc8f52951ee590617990292832fec79899f
Alexandre C Vieira <acamargo.vieira@gmail.com>
2017-10-19 20:31:54 -0200

    Testado icon lookups com sucesso

--- src/launchapps.c
+++ src/launchapps.c
@@ -54,12 +54,12 @@ gchar *lapps_icon = "launchapps.png";
 gchar *wallpaper_conf_url;
 gchar *lapps_wallpaper;
 gboolean running = FALSE;
+gint icon_size;
 
 typedef struct {
 	LXPanel *panel;
 	config_setting_t *settings;
 	GtkWidget *icon_image;
-	// gint icon_size;
 	// char *version;
 } LaunchAppsPlugin;
 
@@ -96,21 +96,21 @@ static void lapps_get_wallpaper_conf_path() {
 	g_free(homedir);
 }
 
-/*static void lapps_set_icons_size(LaunchAppsPlugin *lapps) {
+static void lapps_set_icons_size() {
 	GdkScreen *screen = gdk_screen_get_default();
 	gint s_height = gdk_screen_get_height(screen);
 	gint s_width = gdk_screen_get_width(screen);
 	double suggested_size = (pow(s_width * s_height, ((double) (1.0 / 3.0))) / 1.6);
 	if (suggested_size < 27) {
-		lapps->icon_size = 24;
+		icon_size = 24;
 	} else if (suggested_size >= 27 && suggested_size < 40) {
-		lapps->icon_size = 32;
+		icon_size = 32;
 	} else if (suggested_size >= 40 && suggested_size < 56) {
-		lapps->icon_size = 48;
+		icon_size = 48;
 	} else if (suggested_size >= 56) {
-		lapps->icon_size = 64;
+		icon_size = 64;
 	}
-}*/
+}
 
 static void lapps_main_window_close(GtkWidget *widget, GdkEventButton *event, gpointer user_data)
 {
@@ -128,6 +128,16 @@ static void lapps_exec(GtkWidget *widget, GdkEventButton *event, gpointer user_d
 	GAppInfo *app = g_app_info_dup((GAppInfo *) user_data);
 	g_app_info_launch(app, NULL, NULL, NULL);
 	g_free(app);
+}
+
+static gchar *lapps_icon_filename(GAppInfo *appinfo) {
+	gchar *filename;
+	GtkIconInfo *icon_info;
+	GIcon *g_icon = g_app_info_get_icon(G_APP_INFO(appinfo));
+	GtkIconTheme *icon_theme = gtk_icon_theme_get_default();
+	icon_info = gtk_icon_theme_lookup_by_gicon(icon_theme, g_icon, icon_size, GTK_ICON_LOOKUP_USE_BUILTIN);
+	filename = g_strdup(gtk_icon_info_get_filename(icon_info));
+	return filename;
 }
 
 static void lapps_create_main_window() {
@@ -153,27 +163,53 @@ static void lapps_create_main_window() {
 	gtk_layout_put(GTK_LAYOUT(layout), image, 0, 0);
 
 	// box
-	GtkWidget *box = gtk_hbox_new(TRUE, 1);
+	//GtkWidget *box = gtk_hbox_new(TRUE, 1);
+	GtkWidget *box = gtk_vbox_new(TRUE, 1);
 
 	// list
-	GtkWidget *list = gtk_list_new();
+	// GtkWidget *list = gtk_list_new();
+
+	enum {
+	  PIXBUF_COLUMN,
+	  TEXT_COLUMN,
+	  DATA_COLUMN
+	};
+
+	GtkWidget *icon_view;
+	GtkListStore *store;
+	GtkTreeIter iter;
+	GdkPixbuf *pixbuf;
+
+	icon_view = gtk_icon_view_new ();
+	store = gtk_list_store_new (3, GDK_TYPE_PIXBUF, G_TYPE_STRING, G_TYPE_POINTER);
+
+	gtk_icon_view_set_pixbuf_column (GTK_ICON_VIEW (icon_view), PIXBUF_COLUMN);
+	gtk_icon_view_set_text_column (GTK_ICON_VIEW (icon_view), TEXT_COLUMN);
+	gtk_icon_view_set_model (GTK_ICON_VIEW (icon_view), GTK_TREE_MODEL (store));
+
 	GList *app_list = g_app_info_get_all();
 	GList *l;
-	GtkWidget *item;
+	// GtkWidget *item;
 	for (l = app_list; l != NULL; l = l->next) {
 		if (g_app_info_get_icon(l->data) != NULL) {
-			item = gtk_list_item_new_with_label(
+			gtk_list_store_append (store, &iter);
+			  pixbuf = gdk_pixbuf_new_from_file (lapps_icon_filename(l->data), NULL);
+			  gtk_list_store_set (store, &iter, PIXBUF_COLUMN, pixbuf, TEXT_COLUMN, g_strdup(g_app_info_get_display_name(l->data)), -1);
+			  g_object_unref (pixbuf);
+			/*item = gtk_list_item_new_with_label(
 					g_strconcat(g_app_info_get_id(l->data), " - ", g_app_info_get_name(l->data), " - ",
 							g_app_info_get_display_name(l->data), " - ", g_icon_to_string(g_app_info_get_icon(l->data)),
 							NULL));
 			g_signal_connect(G_OBJECT(item), "button-press-event", G_CALLBACK(lapps_exec), (gpointer)l->data);
 			g_signal_connect(G_OBJECT(item), "button-release-event", G_CALLBACK(lapps_item_clicked_window_close), NULL);
-			gtk_container_add(GTK_CONTAINER(list), item);
+			gtk_container_add(GTK_CONTAINER(list), item);*/
 		}
 	}
-	gtk_box_pack_start(GTK_BOX(box), list, 0, 0, 0);
+	//gtk_box_pack_start(GTK_BOX(box), list, 0, 0, 0);
 
 	// add box to layout
+	//gtk_container_add(GTK_CONTAINER(layout), box);
+	gtk_container_add(GTK_CONTAINER(box), icon_view);
 	gtk_container_add(GTK_CONTAINER(layout), box);
 	gtk_widget_show_all(window);
 	gtk_main();
@@ -220,6 +256,8 @@ static GtkWidget *lapps_constructor(LXPanel *panel, config_setting_t *settings) 
 
 	if (wallpaper_conf_url == NULL)
 		lapps_get_wallpaper_conf_path();
+
+	lapps_set_icons_size();
 
 	return p;
 }
